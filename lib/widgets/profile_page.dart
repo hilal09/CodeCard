280f611f455136788e5303ac4e9b35ebbe527e4e
hilal09 <hilal.cubukcu02@gmail.com>
2023-11-28 04:21:18 +0100

    profile page & settings

---
+++ lib/widgets/profile_page.dart
@@ -0,0 +1,31 @@
+import 'package:codecard/widgets/settings_page.dart';
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(title),
+      ),
+      body: Center(
+        //funktioniert noch nicht
+        child: ElevatedButton(
+          child: const Text('Settings'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const SettingsPage(
+                        title: 'settings',
+                      )),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/settings_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(title),
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Back'),
+        ),
+      ),
+    );
+  }
+}
