81e2e1a48b33a6861f7cce94b95b3192ca6dbf4f
hilal09 <hilal.cubukcu02@gmail.com>
2023-12-06 21:08:25 +0100

    profil

--- lib/widgets/profile_page.dart
+++ lib/widgets/profile_page.dart
@@ -2,21 +2,93 @@ import 'package:codecard/widgets/settings_page.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({Key? key, required this.title}) : super(key: key);
-  final String title;
+  const ProfilePage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(title),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('Settings'),
-          onPressed: () {},
+    return Column(
+      children: [
+        SizedBox(height: 20),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [],
+          ),
         ),
-      ),
+        SizedBox(height: 20),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 500,
+                decoration: BoxDecoration(
+                  color: Colors.grey[800],
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(height: 20),
+        Expanded(
+          child: Row(
+            children: [
+              Container(
+                width: 250,
+                color: Colors.grey[800],
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const SizedBox(height: 20),
+                    Container(
+                      height: 60,
+                      color: Colors.blue,
+                      child: const Center(
+                        child: Text(
+                          'CodeCard',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontFamily: 'YourFont',
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    ListTile(
+                      leading: Icon(Icons.dashboard, color: Colors.white),
+                      title: Text('Profil',
+                          style: TextStyle(fontSize: 16, color: Colors.white)),
+                      onTap: () {
+                        // Aktion beim klicken von Dashboard Widget
+                      },
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.widgets, color: Colors.white),
+                      title: Text('Stapel',
+                          style: TextStyle(fontSize: 16, color: Colors.white)),
+                      onTap: () {
+                        // Aktion bei klicken auf Stapel Widget
+                      },
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.add, color: Colors.white),
+                      title: Text('Hinzufügen',
+                          style: TextStyle(fontSize: 16, color: Colors.white)),
+                      onTap: () {
+                        // Aktion bei klicken auf Hinzufügen Widget
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
