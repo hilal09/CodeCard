a2e6b8e4cef71c1eb23bbf8c4a8e6cefb7b2f638
Amara0123 <amaraakram@msn.com>
2023-12-14 13:26:07 +0100

    logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:codecard/pages/profile_page.dart';
 import 'package:codecard/pages/settings_page.dart';
-import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:codecard/pages/register_page.dart';
 import 'package:codecard/pages/login_page.dart';
@@ -15,13 +15,83 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        textTheme: GoogleFonts
-            .sourceCodeProTextTheme(), // Hier wird die Schriftart angewendet
-      ),
-        home: SplashScreen(), //  Splash Screen zuerst 
+      home: SplashScreen(),
+      theme: ThemeData.dark(),
     );
   }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    _animation = Tween<double>(begin: 0, end: 1).animate(_controller)
+      ..addListener(() {
+        setState(() {});
+      });
+
+    _controller.forward(); //  Animation, sobald der Screen erstellt wird
+
+    // Verzögerung von 3 Sekunden
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => LoginPage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black, // Hintergrundfarbe des Splash Screens
+      body: Center(
+        child: Opacity(
+          opacity: _animation.value,
+          child: Text(
+            '</> CodeCard',
+            style: TextStyle(
+              fontSize: 55,
+              color: Colors.tealAccent,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose(); // Ressourcen freigeben
+    super.dispose();
+  }
+}
+
+@override
+Widget build(BuildContext context) {
+  return MaterialApp(
+    theme: ThemeData(
+      textTheme: GoogleFonts
+          .sourceCodeProTextTheme(), // Hier wird die Schriftart angewendet
+    ),
+    home: Scaffold(
+      body: Dashboard(),
+    ),
+  );
 }
 
 class Dashboard extends StatelessWidget {
