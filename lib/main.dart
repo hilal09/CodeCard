35918dff6a851dc4bb891682c3ec0f9fe50fe317
ceydasar <153534748+ceydasar@users.noreply.github.com>
2023-12-20 14:59:00 +0100

    klassen

--- lib/main.dart
+++ lib/main.dart
@@ -21,12 +21,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         textTheme: GoogleFonts.sourceCodeProTextTheme().copyWith(),
       ),
       home: Scaffold(
-        body: FlashcardPage(), //home: const SplashScreen(),
+        body: DashboardPage(), //home: const SplashScreen(),
       ),
     );
   }
