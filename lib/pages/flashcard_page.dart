402f6e2e3c78ac5714c65068b97842e6a40ccf0c
hilal09 <hilal.cubukcu02@gmail.com>
2023-12-24 21:57:04 +0100

    flashcardpage

--- lib/pages/flashcard_page.dart
+++ lib/pages/flashcard_page.dart
@@ -34,128 +34,161 @@ class _FlashcardPageState extends State<FlashcardPage> {
     await showDialog(
       context: context,
       builder: (BuildContext context) {
-        return Dialog(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16),
+        return AlertDialog(
+          title: Text(
+            existingFlashcard == null
+                ? "Karteikarte erstellen"
+                : "Karteikarte bearbeiten",
+            style: TextStyle(color: Colors.white),
           ),
-          elevation: 0,
-          child: Container(
-            width: 1000,
-            padding: EdgeInsets.all(16),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  'Neue Karteikarte hinzufügen',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 20),
-                TextField(
-                  controller: frontController,
-                  style: TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
-                    labelText: 'Vorderseite',
-                    labelStyle: TextStyle(color: Colors.white),
-                  ),
-                ),
-                SizedBox(height: 10),
-                TextField(
-                  controller: backController,
-                  style: TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
-                    labelText: 'Rückseite',
-                    labelStyle: TextStyle(color: Colors.white),
-                  ),
-                ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: Text('Abbrechen'),
+          content: Container(
+            width: MediaQuery.of(context).size.width * 0.5,
+            child: Form(
+              key: formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextFormField(
+                    controller: frontCaptionController,
+                    maxLength: 35,
+                    onChanged: (value) {
+                      setState(() {
+                        formKey.currentState?.validate();
+                      });
+                      frontCaption = value;
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Bitte gib einen Text für die Vorderseite ein.';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Text für die Vorderseite',
+                      counterText: "",
+                      errorText: formKey.currentState?.validate() == false
+                          ? 'Bitte gib einen Text für die Vorderseite ein.'
+                          : null,
+                      labelStyle: TextStyle(color: Colors.white),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      hintStyle: TextStyle(color: Colors.white),
+                      suffixStyle: TextStyle(color: Colors.white),
                     ),
-                    SizedBox(width: 10),
-                    ElevatedButton(
-                      onPressed: () {
-                        //logik karteikarte speichern
-                        String frontText = frontController.text;
-                        String backText = backController.text;
-                        print('Vorderseite: $frontText, Rückseite: $backText');
-                        Navigator.of(context).pop();
-                      },
-                      child: Text('Bestätigen'),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(height: 10),
+                  TextFormField(
+                    controller: backCaptionController,
+                    maxLength: 35,
+                    onChanged: (value) {
+                      setState(() {
+                        formKey.currentState?.validate();
+                      });
+                      backCaption = value;
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Bitte gib einen Text für die Rückseite ein.';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Text für die Rückseite',
+                      counterText: "",
+                      errorText: formKey.currentState?.validate() == false
+                          ? 'Bitte gib einen Text für die Rückseite ein.'
+                          : null,
+                      labelStyle: TextStyle(color: Colors.white),
+                      focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      hintStyle: TextStyle(color: Colors.white),
+                      suffixStyle: TextStyle(color: Colors.white),
                     ),
-                  ],
-                ),
-              ],
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  SizedBox(height: 10),
+                  Text("Farbe auswählen:",
+                      style: TextStyle(color: Colors.white)),
+                  SizedBox(height: 5),
+                  ColorPicker(
+                    onColorSelected: (color) {
+                      selectedColor = color;
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Abbrechen', style: TextStyle(color: Colors.white)),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (formKey.currentState?.validate() == true) {
+                  if (existingFlashcard == null) {
+                    // Erstelle eine neue Karteikarte
+                    setState(() {
+                      flashcards.add(Flashcard(
+                        frontCaption: frontCaption,
+                        backCaption: backCaption,
+                        color: selectedColor,
+                      ));
+                    });
+                  } else {
+                    // Aktualisiere die vorhandene Karteikarte
+                    setState(() {
+                      existingFlashcard.frontCaption = frontCaption;
+                      existingFlashcard.backCaption = backCaption;
+                      existingFlashcard.color = selectedColor;
+                    });
+                  }
+                  Navigator.of(context).pop();
+                }
+              },
+              child: Text('Erstellen', style: TextStyle(color: Colors.white)),
+            ),
+          ],
         );
       },
     );
   }
 
-  Widget _buildFlashcardTile(Flashcard karteikarte) {
-    return GestureDetector(
-      onTap: () {
-        _showCreateFlashcardDialog(existingFlashcard: karteikarte);
+  Future<void> _showDeleteFlashcardDialog(Flashcard karteikarte) async {
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Karteikarte löschen',
+              style: TextStyle(color: Colors.white)),
+          content: Text(
+              'Bist du sicher, dass du die Karteikarte löschen möchtest?',
+              style: TextStyle(color: Colors.white)),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Abbrechen', style: TextStyle(color: Colors.white)),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  flashcards.remove(karteikarte);
+                });
+                Navigator.of(context).pop();
+              },
+              child: Text('Löschen', style: TextStyle(color: Colors.white)),
+            ),
+          ],
+        );
       },
-      child: Stack(
-        children: [
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
-            padding: const EdgeInsets.all(15),
-            decoration: BoxDecoration(
-              color: karteikarte.color,
-              borderRadius: BorderRadius.circular(15),
-            ),
-            child: Align(
-              alignment: Alignment.topRight,
-              child: PopupMenuButton(
-                itemBuilder: (context) => [
-                  PopupMenuItem(
-                    value: 1,
-                    child: Text("Bearbeiten"),
-                  ),
-                  PopupMenuItem(
-                    value: 2,
-                    child: Text("Löschen"),
-                  ),
-                ],
-                onSelected: (value) {
-                  if (value == 1) {
-                    _showCreateFlashcardDialog(existingFlashcard: karteikarte);
-                  } else if (value == 2) {
-                    _showDeleteFlashcardDialog(karteikarte);
-                  }
-                },
-              ),
-            ),
-          ),
-          Center(
-            child: Text(
-              karteikarte.frontCaption,
-              style: const TextStyle(color: Colors.white),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildEmptyFlashcardMessage() {
-    return Center(
-      child: Text(
-        'Klicke auf das +, um eine neue Karteikarte zu erstellen',
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 18,
-        ),
-        textAlign: TextAlign.center,
-      ),
     );
   }
 
@@ -249,6 +282,67 @@ class _FlashcardPageState extends State<FlashcardPage> {
             style: const TextStyle(color: Colors.white),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildFlashcardTile(Flashcard karteikarte) {
+    return GestureDetector(
+      onTap: () {
+        _showCreateFlashcardDialog(existingFlashcard: karteikarte);
+      },
+      child: Stack(
+        children: [
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+            padding: const EdgeInsets.all(15),
+            decoration: BoxDecoration(
+              color: karteikarte.color,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Align(
+              alignment: Alignment.topRight,
+              child: PopupMenuButton(
+                itemBuilder: (context) => [
+                  PopupMenuItem(
+                    value: 1,
+                    child: Text("Bearbeiten"),
+                  ),
+                  PopupMenuItem(
+                    value: 2,
+                    child: Text("Löschen"),
+                  ),
+                ],
+                onSelected: (value) {
+                  if (value == 1) {
+                    _showCreateFlashcardDialog(existingFlashcard: karteikarte);
+                  } else if (value == 2) {
+                    _showDeleteFlashcardDialog(karteikarte);
+                  }
+                },
+              ),
+            ),
+          ),
+          Center(
+            child: Text(
+              karteikarte.frontCaption,
+              style: const TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildEmptyFlashcardMessage() {
+    return Center(
+      child: Text(
+        'Klicke auf das +, um eine neue Karteikarte zu erstellen',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 18,
+        ),
+        textAlign: TextAlign.center,
       ),
     );
   }
