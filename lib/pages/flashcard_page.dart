baaff81caf561b9c910f50b58e446bee2964ad0d
hilal09 <hilal.cubukcu02@gmail.com>
2023-12-24 17:05:40 +0100

    change

--- lib/pages/flashcard_page.dart
+++ lib/pages/flashcard_page.dart
@@ -1,3 +1,4 @@
+import 'package:codecard/widgets/left_sidebar.dart';
 import 'package:flutter/material.dart';
 
 class FlashcardPage extends StatelessWidget {
@@ -80,46 +81,7 @@ class FlashcardPage extends StatelessWidget {
       padding: const EdgeInsets.all(20.0),
       child: Row(
         children: [
-          Container(
-            width: 70,
-            decoration: BoxDecoration(
-              color: Color(0xFFFF2c293a),
-              borderRadius: BorderRadius.circular(10.0),
-              border: Border.all(
-                color: Color.fromARGB(255, 141, 134, 134),
-                width: 0.5,
-              ),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                SizedBox(height: 20),
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(15),
-                  child: Image.asset(
-                    'assets/images/Logo.png',
-                    height: 50,
-                    width: 50,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                SizedBox(height: 10),
-                IconButton(
-                  icon: Icon(Icons.home_rounded, color: Colors.white),
-                  onPressed: () {
-                    // logik home button
-                  },
-                ),
-                SizedBox(height: 10),
-                IconButton(
-                  icon: Icon(Icons.add, color: Colors.white),
-                  onPressed: () {
-                    //logik neues lernset erstellen
-                  },
-                ),
-              ],
-            ),
-          ),
+          LeftSideBar(),
           SizedBox(
             width: 20,
           ),
