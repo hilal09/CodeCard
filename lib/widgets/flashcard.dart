7041ed8dab73bf2fe0448d7f962cbfb9c6b3fb61
Amara0123 <121958512+Amara0123@users.noreply.github.com>
2024-01-08 20:25:06 +0100

    Update flashcard.dart

--- lib/widgets/flashcard.dart
+++ lib/widgets/flashcard.dart
@@ -1,11 +1,17 @@
 /* 
 DateiName: flashcard.dart
-Authors: Hilal Cubukcu(alles, außer id)
+Authors: Hilal Cubukcu(alles, außer id und Box)
 Zuletzt bearbeitet am: 07.01.2024
 Beschreibung: DDieser Dart-Code definiert eine Klasse namens Flashcard. Ein 
 Flashcard-Objekt repräsentiert eine Karte in deiner Anwendung und enthält 
 Informationen wie die Benutzer-ID (userUID), die Vorderseitenbeschriftung 
 (frontCaption), die Rückseitenbeschriftung (backCaption) und die Farbe der Karte (color).
+-`boxNumber`: Ein Schlüsselelement des Leitner-Algorithmus, das die Position der Karteikarte in der Lernbox bestimmt.
+Karten bewegen sich zwischen verschiedenen Boxen basierend auf der Korrektheit der Benutzerantworten, 
+was ein effizientes Wiederholungsschema ermöglicht.
+
+Diese Klasse ist zentral für die Logik, die Karteikarten in der App betrifft, und unterstützt den Leitner-Algorithmus, um den Lernprozess zu optimieren.
+
 */
 import 'package:flutter/material.dart';
 
